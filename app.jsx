6236b8d2ff4c1c49d0434d4c9e8606138dec5b12
karthikeyan6117 <karthikeyan.a.2024.cse@rajalakshmi.edu.in>
2026-04-05 20:53:26 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DuplicateChecker from "./DuplicateChecker";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <DuplicateChecker />
+    </div>
+  );
+}
+
+export default App;
